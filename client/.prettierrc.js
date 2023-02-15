6af99ace6fa43e7dc16fa196da7eb0350ca83ed1
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-02-15 11:40:37 +0300

    initialize

---
+++ client/.prettierrc.js
@@ -0,0 +1,15 @@
+const prettierConfig = {
+  singleQuote: true,
+  tabWidth: 2,
+  printWidth: 80,
+  semi: false,
+  trailingComma: 'none',
+  jsxSingleQuote: true,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'avoid'
+}
+
+module.exports = prettierConfig
---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import("tailwindcss").Config} */
+const tailwindConfig = {
+  content: [
+    './app/**/*.{ts,tsx}',
+  ],
+  plugins: [],
+  theme: {
+    extend: {}
+  }
+}
+
+module.exports = tailwindConfig
