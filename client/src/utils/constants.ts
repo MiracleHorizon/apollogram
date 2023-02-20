8680949a5c5f0723bd653b7efdc513b9250bfc2c
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-02-20 14:13:50 +0300

    fix: process.env -> import.meta.env

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,2 +1,3 @@
 export const APP_NAME = 'Apollogram'
-export const SERVER_API = process.env.SERVER_API ?? 'http://localhost:4200'
+export const SERVER_API: string =
+  import.meta.env.SERVER_API ?? 'http://localhost:4200'
